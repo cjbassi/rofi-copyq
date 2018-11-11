7ef4bb83f7ca0f7d4df28bff47e50b7d25ea0f7c
Caleb Bassi <calebjbassi@gmail.com>
2018-11-10 21:33:41 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='rofi-copyq',
+    version='0.1.0',
+    scripts=['rofi-copyq'],
+    url='https://github.com/cjbassi/rofi-copyq',
+)
